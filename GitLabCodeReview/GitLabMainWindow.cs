e41c0b3a7e9b96054e0422fb94d766ab18c1a595
andriifeseniuk <andrew_fesenyuk@ukr.net>
2020-06-11 18:33:56 +0300

    fixed window caption and button text

--- GitLabCodeReview/GitLabMainWindow.cs
+++ GitLabCodeReview/GitLabMainWindow.cs
@@ -29,7 +29,7 @@ namespace GitLabCodeReview
         /// </summary>
         public GitLabMainWindow() : base(null)
         {
-            this.Caption = "GitLabMainWindow";
+            this.Caption = "GitLab Code Review";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
