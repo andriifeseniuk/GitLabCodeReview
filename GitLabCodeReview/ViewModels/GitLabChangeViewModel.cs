a6252b7e9ad7ff871aa6f926868da43f793e1922
andriifeseniuk <andrew_fesenyuk@ukr.net>
2020-08-13 01:40:21 +0300

    added stub for discussions section

--- GitLabCodeReview/ViewModels/GitLabChangeViewModel.cs
+++ GitLabCodeReview/ViewModels/GitLabChangeViewModel.cs
@@ -1,22 +1,56 @@
 ﻿using GitLabCodeReview.Common.Commands;
 using GitLabCodeReview.Models;
+using System.Windows;
 using System.Windows.Input;
 
 namespace GitLabCodeReview.ViewModels
 {
-    public class GitLabChangeViewModel
+    public class GitLabChangeViewModel : BaseViewModel
     {
+        private const string MoreText = "more";
+        private const string LessText = "less";
         private GitLabChange change;
         private MainViewModel mainViewModel;
+        private Visibility moreSectionVisibility = Visibility.Collapsed;
+        private string moreLessText = MoreText;
 
         public GitLabChangeViewModel(GitLabChange gitLabChange, MainViewModel mainVM)
         {
             this.change = gitLabChange;
             this.mainViewModel = mainVM;
             this.DiffCommand = new DelegateCommand(x => this.ExecuteDiff());
+            this.MoreLessCommand = new DelegateCommand(x => this.ExecuteMoreLess());
         }
 
-        public ICommand DiffCommand { get; } 
+        public ICommand DiffCommand { get; }
+
+        public ICommand MoreLessCommand { get; }
+
+        public Visibility MoreSectionVisibility
+        {
+            get
+            {
+                return this.moreSectionVisibility;
+            }
+            set
+            {
+                this.moreSectionVisibility = value;
+                this.SchedulePropertyChanged();
+            }
+        }
+
+        public string MoreLessText
+        {
+            get
+            {
+                return this.moreLessText;
+            }
+            set
+            {
+                this.moreLessText = value;
+                this.SchedulePropertyChanged();
+            }
+        }
 
         public string FileDisplayName
         {
@@ -44,6 +78,20 @@ namespace GitLabCodeReview.ViewModels
         private void ExecuteDiff()
         {
             this.mainViewModel.ExecuteDiff(this.change);
+        }
+
+        private void ExecuteMoreLess()
+        {
+            if (this.MoreSectionVisibility == Visibility.Visible)
+            {
+                this.MoreSectionVisibility = Visibility.Collapsed;
+                this.MoreLessText = MoreText;
+            }
+            else
+            {
+                this.MoreSectionVisibility = Visibility.Visible;
+                this.MoreLessText = LessText;
+            }
         }
     }
 }
