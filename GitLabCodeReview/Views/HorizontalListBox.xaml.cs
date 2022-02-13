fceb2a2e050a4ef659537192a53b2a27e657af71
andriifeseniuk <andrew_fesenyuk@ukr.net>
2022-02-13 20:53:52 +0200

    separated HorizontalListBox (incomplete)

---
+++ GitLabCodeReview/Views/HorizontalListBox.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GitLabCodeReview.Views
+{
+    /// <summary>
+    /// Interaction logic for HorizontalListBox.xaml
+    /// </summary>
+    public partial class HorizontalListBox : UserControl
+    {
+        public HorizontalListBox()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(nameof(HorizontalListBox.ItemsSource), typeof(IEnumerable), typeof(HorizontalListBox));
+
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register(nameof(HorizontalListBox.SelectedItem), typeof(object), typeof(HorizontalListBox));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public object SelectedItem
+        {
+            get { return GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+    }
+}
